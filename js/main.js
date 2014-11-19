d3f8dac7c8a59b9ece3254ee16b4eece1314543c
gagn0369 <madeleinemagnolia@gmail.com>
2014-11-19 12:19:42 -0500

    fixed javascript and parallax!

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 var $win = $(window);
 var $masthead = $(".masthead");
-var $footer = $(".footer");
+var $nav = $(".nav-container");
+var $footer = $(".contact");
 
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
-  // var footerPos = (scrollPos - footerOffset.top) / 2;
+    var footerPos = (scrollPos - $footer.offset().top) / 2;
 
 	$masthead.css('background-position', 'center ' + scrollPos / 2 + 'px');
 
 	$footer.css('background-position', 'center ' + footerPos + 'px');
+});
+
+$nav.waypoint(function(direction) {
+	if (direction == "down") {
+		$nav.addClass('sticky');
+	} else {
+		$nav.removeClass('sticky');
+	}
 });
